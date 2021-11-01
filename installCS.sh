5ed714f927da9851b71609be95f1cb809b8c319c
2096779623 <57583560+2096779623@users.noreply.github.com>
2021-11-01 13:15:39 +0800

    Update installCS.sh

--- installCS.sh
+++ installCS.sh
@@ -5,7 +5,7 @@ echo "Update And Install Packages..."
 pkg update -y > /dev/null 2>&1 && pkg i x11-repo wget unzip openjdk-17 -y > /dev/null 2>&1 && pkg i tigervnc -y > /dev/null 2>&1
 echo "Downloading CobaltStrike..."
 cd ~ && mkdir CobaltStrike && cd CobaltStrike
-wget -q https://github.com/2096779623/Termux-CobaltStrike-CN/raw/main/CS.zip
+wget https://github.com/2096779623/Termux-CobaltStrike-CN/raw/main/CS.zip
 unzip CS.zip > /dev/null 2>&1
 chmod 777 -R * > /dev/null 2>&1
 echo "You Can Use:"vncserver :1 > /dev/null 2>&1 && java -Dfile.encoding=UTF-8 -javaagent:CobaltStrikeCN.jar -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC  -jar cobaltstrike.jar" Run The CobaltStrike!"
