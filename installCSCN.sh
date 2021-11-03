8f2a306dcc954f88b4745b5bb1b3ec61fe78496f
2096779623 <57583560+2096779623@users.noreply.github.com>
2021-11-03 12:44:57 +0800

    Add files via upload

---
+++ installCSCN.sh
@@ -0,0 +1,13 @@
+#/data/data/com.termux/files/usr/bin/bash
+#By 2096779623
+echo "提示:请不要使用Termux运行teamserver,如果你有root，可以试试!"
+echo "升级和安装软件包..."
+pkg update -y > /dev/null 2>&1 && pkg i x11-repo wget unzip openjdk-17 -y > /dev/null 2>&1 && pkg i tigervnc -y > /dev/null 2>&1
+echo "下载CobaltStrike..."
+cd $HOME > /dev/null 2>&1
+mkdir CobaltStrike > /dev/null 2>&1
+cd ~/CobaltStrike > /dev/null 2>&1
+wget -q https://github.com/2096779623/Termux-CobaltStrike/raw/main/CSCN.zip
+unzip CS.zip > /dev/null 2>&1
+chmod 777 -R * > /dev/null 2>&1
+echo "你可以使用:vncserver :1 > /dev/null 2>&1 &&  java -Dfile.encoding=UTF-8 -javaagent:CobaltStrikeCN.jar -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC  -jar cobaltstrike.jar 来运行CobaltStrike!"
