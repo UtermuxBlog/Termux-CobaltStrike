9151323712af60b29a5632eb0df9a751ce54a44a
2096779623 <57583560+2096779623@users.noreply.github.com>
2021-11-03 12:51:49 +0800

    Update installCSCN.sh

--- installCSCN.sh
+++ installCSCN.sh
@@ -7,7 +7,7 @@ echo "下载CobaltStrike..."
 cd $HOME > /dev/null 2>&1
 mkdir CobaltStrikeCN > /dev/null 2>&1
 cd ~/CobaltStrikeCN > /dev/null 2>&1
-wget -q https://github.com/2096779623/Termux-CobaltStrike/raw/main/CSCN.zip
+wget -q https://hub.fastgit.org/2096779623/Termux-CobaltStrike/raw/main/CSCN.zip
 unzip CSCN.zip > /dev/null 2>&1
 chmod 777 -R * > /dev/null 2>&1
 echo "你可以使用:vncserver :1 > /dev/null 2>&1 &&  java -Dfile.encoding=UTF-8 -javaagent:CobaltStrikeCN.jar -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC  -jar cobaltstrike.jar 来运行CobaltStrike!"
