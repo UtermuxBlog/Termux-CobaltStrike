73d71cc112bd33f50687aca0cf1b2c78a8dee00d
2096779623 <57583560+2096779623@users.noreply.github.com>
2021-11-25 12:55:13 +0800

    Update installCSCN.sh

--- installCSCN.sh
+++ installCSCN.sh
@@ -13,7 +13,7 @@ wget -q https://hub.fastgit.org/2096779623/Termux-CobaltStrike/raw/main/CSCN.zip
 unzip CSCN.zip > /dev/null 2>&1
 chmod 777 -R * > /dev/null 2>&1
 echo 添加启动脚本...
-echo  "vncserver -kill :1 && vncserver :1 && cd ~/CobaltStrikeCN && export DISPLAY=:1 && java -Dfile.encoding=UTF-8 -javaagent:CobaltStrikeCN.jar -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC  -jar cobaltstrike.jar" > $PREFIX/bin/cobaltstrikeCN
+echo  "vncserver -kill :1 && vncserver :1 && cd ~/CobaltStrikeCN && export DISPLAY=:1 && java -Dfile.encoding=UTF-8 -javaagent:CobaltstrikeCN.jar -XX:ParallelGCThreads=4 -XX:+AggressiveHeap -XX:+UseParallelGC  -jar cobaltstrike.jar" > $PREFIX/bin/cobaltstrikeCN
 chmod 777 $PREFIX/bin/cobaltstrikeCN
 echo 你可以执行:cobaltstrikeCN   来启动cobaltstrike！
 
