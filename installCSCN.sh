360ff823bb61815c9e7e5b4cdc9308a6f0278372
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-04-03 17:38:31 +0800

    Update installCSCN.sh

--- installCSCN.sh
+++ installCSCN.sh
@@ -1,6 +1,6 @@
 #/data/data/com.termux/files/usr/bin/bash
 #By 2096779623
-echo "警告:请不要使用Termux运行teamserver,如果你有root，可以试试!"
+echo "teamserver需要root权限才能运行！"
 echo "升级和安装软件包..."
 pkg update -y > /dev/null 2>&1 && pkg i x11-repo wget unzip openjdk-17 -y > /dev/null 2>&1 && pkg i tigervnc -y > /dev/null 2>&1
 echo "下载CobaltStrike..."
